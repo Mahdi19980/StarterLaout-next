80e97b5b76060334eef2ca3af62079b9a2d9c610
Mahdi19980 <mahdialimohammadi73@gmail.com>
2023-06-10 17:51:31 +0330

    some changes

--- app/components/drawer.jsx
+++ app/components/drawer.jsx
@@ -9,38 +9,40 @@ const SideBar = () => {
       >
         {/* Sidebar content here */}
 
-        <li className="flex menu-title  justify-between text-justify mb-8 shadow-md">
-          <div className="">
-            <label className=" swap swap-rotate">
-              {/* this hidden checkbox controls the state */}
-              <input type="checkbox" />
+        <li className=" menu-title justify-between pl-0 text-justify mb-8 shadow-md">
+          <div className="flex items-start ">
+            <div className="flex  items-center">
+              <p className="ui font-bold">UI</p>
+              <p className="daisy font-bold">daisy</p>
+            </div>
+            <div className="mr-8">
+              <label className=" swap swap-rotate">
+                {/* this hidden checkbox controls the state */}
+                <input type="checkbox" />
 
-              {/* hamburger icon */}
-              <svg
-                className="swap-off fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                viewBox="0 0 512 512"
-              >
-                <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
-              </svg>
+                {/* hamburger icon */}
+                <svg
+                  className="swap-off fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+                </svg>
 
-              {/* close icon */}
-              <svg
-                className="swap-on fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                viewBox="0 0 512 512"
-              >
-                <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
-              </svg>
-            </label>
-          </div>
-          <div className="flex  items-center">
-            <p className="daisy font-bold">daisy</p>
-            <p className="ui font-bold">UI</p>
+                {/* close icon */}
+                <svg
+                  className="swap-on fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 512 512"
+                >
+                  <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+                </svg>
+              </label>
+            </div>
           </div>
         </li>
 
