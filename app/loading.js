283452625009372c66726b65b9fe18021e1537e8
Mahdi19980 <mahdialimohammadi73@gmail.com>
2023-06-21 19:31:17 +0330

    this is a some change

---
+++ app/loading.js
@@ -0,0 +1,14 @@
+const Loading = () => {
+  return (
+    <div className="loadingio-spinner-ellipsis-guxg0zs71w7">
+      <div className="ldio-rnzaqz0j4q">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+export default Loading;
